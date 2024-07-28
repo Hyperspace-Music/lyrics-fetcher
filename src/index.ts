608870596d7e20c922ac213d90ec61dc851348da
purry03 <rohansh.lko@gmail.com>
2024-07-28 13:43:41 +0530

    Dockerized app

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,6 @@ const logger = winston.createLogger({
     ),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'app.log' })
     ]
 });
 
